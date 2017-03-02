8af5fcaedf8defd2c2c9e018fb677a52c8ef7c92
Jorgepastorr <jorge.pastor.r@hotmail.com>
2017-03-02 13:43:50 +0100

    Create ejercicio-eded-1-2.py

---
+++ python/ejercicio-eded-1-2.py
@@ -0,0 +1,13 @@
+#!/bin/python
+# coding: UTF-8
+
+edad = input("Que vedad tienes? ")
+
+if edad >= 15 and edad <=17 :
+	print "Puede ventrar en sesion de tarde"
+	
+elif edad >= 18 and edad <= 23 :
+	print "Puede entrar en la sesión de jovenes"
+	
+else:
+	print "No cumple los requisitos"
