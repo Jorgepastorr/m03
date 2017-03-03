42fd3aadf5ca7f56a733b7525955a0e8ca16b33c
root <root@a03.informatica.escoladeltreball.org>
2017-03-03 13:08:12 +0100

    ver2

---
+++ python/ejercicios/ejercicio-eded-3.py
@@ -0,0 +1,10 @@
+#!/bin/python
+# coding: UTF-8
+
+edad = input("Que vedad tienes? ")
+	
+if (edad >= 18 and edad <= 23) or  edad == 17  :
+	print "Puede entrar en la sesión de jovenes"
+	
+else:
+	print "No cumple los requisitos"
