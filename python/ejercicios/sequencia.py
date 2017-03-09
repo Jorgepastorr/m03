318cbf13dde93602435ad4d6fe95c9cfb5a30894
Jorgepastorr <jorge.pastor.r@hotmail.com>
2017-03-09 21:04:02 +0100

    Add files via upload

---
+++ python/ejercicios/sequencia.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#SWAP. Intercambio de variables
+'''
+der="movil"
+izq="bocadillo"
+der ,izq = izq, der
+
+print der
+print izq
+'''
+
+
+# secuencia solo pares
+'''
+var = input("num: ")
+for   i in range(1,var) :
+	if i %2==0:
+		print (i)
+'''	
+
+# cuenta secuencia de 0 al 50 de 5 en 5
+'''
+for   i in range(0,51,5) :
+	print (i)
+'''
