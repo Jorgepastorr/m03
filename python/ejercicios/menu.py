4882d759e2d15c9bfefcd7ef5c432670956b5afa
jorge <jorge@localhost.localdomain>
2017-03-14 22:19:32 +0100

    img-escola

--- python/ejercicios/menu.py
+++ python/ejercicios/menu.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import os
-salir = raw_input("")
+salir = False
 
-while salir False:
+while not salir:
 
 	os.system('clear')
-	var = input(" 1-Sumar\n 2-Restar\n 3-salir\n Que desea hacer: ")
+	var = raw_input(" 1-Sumar\n 2-Restar\n 3-salir\n Que desea hacer: ")
 
 
-	if var == 1:
+	if var == "1":
 		print "opcion 1 suma"
 		tecla = raw_input("Pulse enter para continuar")
 	
-	elif var == 2:
+	elif var == "2":
 		print "opcion 2 resta"
 		tecla = raw_input("Pulse enter para continuar")
-	elif var == 3:
+	
+	elif var == "3":
 		salir = True
+	
 	else:
 		print "ninguna opcion correcta"
 		tecla = raw_input("Pulse enter para continuar")
