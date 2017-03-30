3ca18d1e591363219c563778b6081acf340365d4
jorge <jorge@localhost.localdomain>
2017-03-30 18:58:08 +0200

    30-03-17

---
+++ python/entregas/30-03-17/pi-pa-ti.md
@@ -0,0 +1,20 @@
+**Piedra, palel, tijera**
+
+```python
+print "opciones del juego"
+pl1 = read ()
+pl2 = read ()
+
+if opciones iguales then
+    print "empate"
+eif
+
+if opciones gana pl1 then
+    print "gana player 1"
+
+else then
+   print "gana player 2"
+eif
+```
+
+
---
+++ python/entregas/30-03-17/pi-pa-ti.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print "piedra, patel, tijera"
+print "      pi/pa/ti"
+pl1 = raw_input("player 1: ")
+pl2 = raw_input("player 2: ")
+
+if ( pl1 != "pi" and pl1 != "pa" and pl1 != "ti") or ( pl2 != "pi" and pl2 != "pa" and pl2 != "ti" ) :
+	print "¡opciones incorrectas!"
+	
+elif ( pl1 == "pi" and pl2 == "pi" ) or ( pl1 == "pa" and pl2 == "pa" ) or ( pl1 == "ti" and pl2 == "ti" ):
+	   print pl1,"y",pl2," Empate "
+
+elif ( pl1 == "pi" and pl2 == "ti" ) or ( pl1 == "pa" and pl2 == "pi" ) or   ( pl1 == "ti" and pl2 == "pa" ):
+		 print pl1,"y",pl2,"Gana",pl1,"player 1"
+else:
+	print pl1,"y",pl2,"Gana",pl2,"player 2"
