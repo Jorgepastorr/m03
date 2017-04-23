0b51806ae574798bd8eb1c6c0849c54d519ee97f
jorge <jorge@localhost.localdomain>
2017-04-23 21:37:04 +0200

    '2017-04-23'

--- python/exemples-def/Ejemplo_funcion-booleano.py
+++ python/exemples-def/Ejemplo_funcion-booleano.py
@@ -1,4 +1,11 @@
 # coding:utf-8
+# ---------------------------------------------------------------
+# a               : entrada
+# resultado       : salida 
+
+# una vez dentro del def puedes cambiarle el nombre a la variable
+# al salir del def seguiran teniendo el nombre anterior  
+# ---------------------------------------------------------------
 def es_par(a):
 	resultado=False
 	
@@ -13,7 +20,9 @@ def es_par(a):
 num=0
 
 num=input("Introduce un número:")
-
+# ---------------------------------------------------------------
+# num               : entrada
+# ---------------------------------------------------------------
 if es_par(num):
 	print "Es par"
 else:
