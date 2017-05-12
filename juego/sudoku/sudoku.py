ed5cad9e8b9c646e191242cbb8227d31a7406256
jorge <jorge@localhost.localdomain>
2017-05-12 20:28:00 +0200

    '2017-05-12'

--- juego/sudoku/sudoku.py
+++ juego/sudoku/sudoku.py
@@ -7,12 +7,17 @@
 import os
 from time import sleep
 os.system('clear')
-#################################
+###################################################################################################
 #       variables
-####################################3
+######################################################################################################
+salir = False
+numero=0
+fila=20
+columna=20
+casilla=""
 
 ############ listas ###############
-## listas de lineas y recuadros
+## listas de filas, columnas y recuadros
 A,B,C,D,E,F,G,H,I=[8,0,0,0,0,0,0,0,0],[9,5,6,7,4,0,0,0,0],[3,9,0,0,0,0,0,0,0],[2,0,0,0,0,0,0,0,0],[4,5,0,0,0,0,0,0,0],[7,1,2,9,4,0,0,0,0],[6,8,2,7,3,0,0,0,0],[4,5,6,8,0,0,0,0,0],[4,2,6,3,9,5,0,0,0]
 l1,l2,l3,l4,l5,l6,l7,l8,l9=[9,3,7,4,0,0,0,0,0],[8,5,2,1,6,0,0,0,0],[4,8,2,0,0,0,0,0,0],[4,6,0,0,0,0,0,0,0],[6,2,5,3,0,0,0,0,0],[5,2,0,0,0,0,0,0,0],[7,9,6,0,0,0,0,0,0],[4,7,9,0,0,0,0,0,0],[4,9,3,8,5,0,0,0,0]
 r1,r2,r3,r4,r5,r6,r7,r8,r9=[8,9,5,3,0,0,0,0,0],[0,6,0,0,0,0,0,0,0],[7,4,9,0,0,0,0,0,0],[2,4,7,1,0,0,0,0,0],[5,2,0,0,0,0,0,0,0],[9,4,0,0,0,0,0,0,0],[0,6,8,4,2,0,0,0,0],[2,4,5,6,3,0,0,0,0],[7,3,6,8,9,5,0,0,0]
@@ -31,15 +36,19 @@ g1,g2,g3,g4,g5,g6,g7,g8,g9=0,6,8,0,2,0,0,7,3
 h1,h2,h3,h4,h5,h6,h7,h8,h9=0,0,0,4,5,0,6,0,8
 i1,i2,i3,i4,i5,i6,i7,i8,i9=4,0,2,6,3,0,0,9,5
 
-#################################
 
-###################################3
+########### diccionarios
+# diccionario columna asigna el numero de columna con su lista
 lista_columna={"1":l1,"2":l2,"3":l3,"4":l4,"5":l5,"6":l6,"7":l7,"8":l8,"9":l9}
+
+# diccionario fila asigna el numero de columna con su lista
 lista_fila={"a":A,"b":B,"c":C,"d":D,"e":E,"f":F,"g":G,"h":H,"i":I}
+
+# diccionario de col y fil a numerico ( Es para facilitar control de errores a la asignación) 
 fila_cambio_a_numero={"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9}
 columna_a_num={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9}
 
-# DICCIONARIO asignación
+# DICCIONARIO asignación de raw a variable
 asignacion={"a1":a1,"a2":a2,"a3":a3,"a4":a4,"a5":a5,"a6":a6,"a7":a7,"a8":a8,"a9":a9,"b1":b1,"b2":b2,"b3":b3,"b4":b4,"b5":b5,"b6":b6,"b7":b7,"b8":b8,"b9":b9,"c1":c1,"c2":c2,"c3":c3,"c4":c4,"c5":c5,"c6":c6,"c7":c7,"c8":c8,"c9":c9,"d1":d1,"d2":d2,"d3":d3,"d4":d4,"d5":d5,"d6":d6,"d7":d7,"d8":d8,"d9":d9,"e1":e1,"e2":e2,"e3":e3,"e4":e4,"e5":e5,"e6":e6,"e7":e7,"e8":e8,"e9":e9,"f1":f1,"f2":f2,"f3":f3,"f4":f4,"f5":f5,"f6":f6,"f7":f7,"f8":f8,"f9":f9,"g1":g1,"g2":g2,"g3":g3,"g4":g4,"g5":g5,"g6":g6,"g7":g7,"g8":g8,"g9":g9,"h1":h1,"h2":h2,"h3":h3,"h4":h4,"h5":h5,"h6":h6,"h7":h7,"h8":h8,"h9":h9,"i1":i1,"i2":i2,"i3":i3,"i4":i4,"i5":i5,"i6":i6,"i7":i7,"i8":i8,"i9":i9}
 
 
@@ -47,7 +56,7 @@ asignacion={"a1":a1,"a2":a2,"a3":a3,"a4":a4,"a5":a5,"a6":a6,"a7":a7,"a8":a8,"a9"
 #################################
 #  niveles def
 #################################
-def control_casillas_originales(salir_raw):
+def control_casillas_originales(salir_input):
 
 	#### variables salia sudoku 1
 	a1,a2,a3,a4,a5,a6,a7,a8,a9=0,8,0,0,0,0,0,0,0
@@ -59,23 +68,29 @@ def control_casillas_originales(salir_raw):
 	g1,g2,g3,g4,g5,g6,g7,g8,g9=0,6,8,0,2,0,0,7,3
 	h1,h2,h3,h4,h5,h6,h7,h8,h9=0,0,0,4,5,0,6,0,8
 	i1,i2,i3,i4,i5,i6,i7,i8,i9=4,0,2,6,3,0,0,9,5
-	
+	# diccionario para comprobar que introduces bien filas y columnas
 	asignacion={"a1":a1,"a2":a2,"a3":a3,"a4":a4,"a5":a5,"a6":a6,"a7":a7,"a8":a8,"a9":a9,"b1":b1,"b2":b2,"b3":b3,"b4":b4,"b5":b5,"b6":b6,"b7":b7,"b8":b8,"b9":b9,"c1":c1,"c2":c2,"c3":c3,"c4":c4,"c5":c5,"c6":c6,"c7":c7,"c8":c8,"c9":c9,"d1":d1,"d2":d2,"d3":d3,"d4":d4,"d5":d5,"d6":d6,"d7":d7,"d8":d8,"d9":d9,"e1":e1,"e2":e2,"e3":e3,"e4":e4,"e5":e5,"e6":e6,"e7":e7,"e8":e8,"e9":e9,"f1":f1,"f2":f2,"f3":f3,"f4":f4,"f5":f5,"f6":f6,"f7":f7,"f8":f8,"f9":f9,"g1":g1,"g2":g2,"g3":g3,"g4":g4,"g5":g5,"g6":g6,"g7":g7,"g8":g8,"g9":g9,"h1":h1,"h2":h2,"h3":h3,"h4":h4,"h5":h5,"h6":h6,"h7":h7,"h8":h8,"h9":h9,"i1":i1,"i2":i2,"i3":i3,"i4":i4,"i5":i5,"i6":i6,"i7":i7,"i8":i8,"i9":i9}
+	
 	fila= raw_input("Inserta letra de fila ( Siempre en minusculas ): ")
 	columna=raw_input("Inserta número de columna: ")
 	# jumnta 2 variables en 1
 	casilla=(fila+columna)
 	
-	# si la casilla es diferente a 0 no te deja insertar el númeo
-	if asignacion[casilla] != 0 :
+	# SI NO EXISTE la variables de fila y columna en el diccionario muestra error
+	if casilla not in asignacion :
+		print "Esa asignación fila columna incorrecta" 
+		sleep (3)				
+	# si la casilla es diferente a 0 no te deja insertar el númeo (es para que no puedas ambiar los números iniciales)
+	elif asignacion[casilla] != 0 :
 		print "No puedes cambiar ese número. \nEs del sudoku original."
+		sleep (3)				
 	else:	
-		salir_raw=True
+		salir_input=True
 
-	return fila,columna,casilla,salir_raw
+	return fila,columna,casilla,salir_input
 	
 
-############### asignacion buena ##################
+############### asignacion  ##################
 
 def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambio_a_numero,columna_a_num,asignacion,salir):
 	
@@ -84,7 +99,7 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 		if ((columna_a_num[columna] >= 1 and columna_a_num[columna] <=3 ) and
 			( fila_cambio_a_numero[fila] >= 1 and fila_cambio_a_numero[fila] <= 3 )): 
 			if not (numero in r1 ):
-				# quitar el anterior
+				# quitar el anterior numero de la casilla
 				lista_fila[fila].remove(asignacion[casilla])
 				lista_columna[columna].remove(asignacion[casilla])
 				r1.remove(asignacion[casilla])
@@ -94,7 +109,11 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				r1.insert(1,numero)
 				#
 				asignacion[casilla]=numero
-				
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
+						
 		elif ((columna_a_num[columna] >= 4 and columna_a_num[columna] <=6 ) and
 			( fila_cambio_a_numero[fila] >= 1 and fila_cambio_a_numero[fila] <= 3 )): 
 			if not (numero in r2 ):
@@ -107,7 +126,11 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r2.insert(1,numero)
 				asignacion[casilla]=numero
-				
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
+					
 		elif ((columna_a_num[columna] >= 7 and columna_a_num[columna] <=9 ) and
 			( fila_cambio_a_numero[fila] >= 1 and fila_cambio_a_numero[fila] <= 3 )): 
 			if not (numero in r3 ):
@@ -120,7 +143,11 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r3.insert(1,numero)
 				asignacion[casilla]=numero
-				
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
+					
 		elif ((columna_a_num[columna] >= 1 and columna_a_num[columna] <=3 ) and
 			( fila_cambio_a_numero[fila] >= 4 and fila_cambio_a_numero[fila] <= 6 )): 
 			if not (numero in r4 ):
@@ -133,7 +160,11 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r4.insert(1,numero)
 				asignacion[casilla]=numero
-				
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
+			
 		elif ((columna_a_num[columna] >= 4 and columna_a_num[columna] <=6 ) and
 			( fila_cambio_a_numero[fila] >= 4 and fila_cambio_a_numero[fila] <= 6 )): 
 			if not (numero in r5 ):
@@ -146,6 +177,10 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r5.insert(1,numero)
 				asignacion[casilla]=numero
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
 				
 		elif ((columna_a_num[columna] >= 7 and columna_a_num[columna] <=9 ) and
 			( fila_cambio_a_numero[fila] >= 4 and fila_cambio_a_numero[fila] <= 6 )): 
@@ -159,6 +194,10 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r6.insert(1,numero)
 				asignacion[casilla]=numero
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
 				
 		elif ((columna_a_num[columna] >= 1 and columna_a_num[columna] <=3 ) and
 			( fila_cambio_a_numero[fila] >= 7 and fila_cambio_a_numero[fila] <= 9 )): 
@@ -172,6 +211,10 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r7.insert(1,numero)
 				asignacion[casilla]=numero
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
 				
 		elif ((columna_a_num[columna] >= 4 and columna_a_num[columna] <=6 ) and
 			( fila_cambio_a_numero[fila] >= 7 and fila_cambio_a_numero[fila] <= 9 )): 
@@ -185,11 +228,15 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r8.insert(1,numero)
 				asignacion[casilla]=numero
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)
 				
 		elif ((columna_a_num[columna] >= 7 and columna_a_num[columna] <=9 ) and
 			( fila_cambio_a_numero[fila] >= 7 and fila_cambio_a_numero[fila] <= 9 )): 
 			if not (numero in r9 ):
-				# quitar el anterior
+				# quitar el anterior numero de la casilla
 				lista_fila[fila].remove(asignacion[casilla])
 				lista_columna[columna].remove(asignacion[casilla])
 				r9.remove(asignacion[casilla])
@@ -198,7 +245,10 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 				lista_columna[columna].insert(1,numero)
 				r9.insert(1,numero)
 				asignacion[casilla]=numero
-							
+			else:
+				os.system('clear')
+				print "Ese número ya existe en ese recuadro \nNo puede colocarse ahí"
+				sleep (4)				
 	else:
 		os.system('clear')
 		print "Ese número ya existe en una linea o recuadro \nNo puede colocarse ahí"
@@ -207,10 +257,6 @@ def asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambi
 	return lista_columna,lista_fila,fila_cambio_a_numero,columna_a_num,asignacion,salir 
 
 
-
-#################################
-#################################
-#################################
 ################################
 ##############   pantalla de sudoku inicial ########
 
@@ -242,35 +288,7 @@ def sudoku(asignacion ):
 
 
 #################################
-def condicion_entrada(fila,numero,columna,fila_cambio_a_numero,columna_a_num):
-
-	if not ((fila in fila_cambio_a_numero ) and
-			(columna in columna_a_num )):
-		entrada= False
-	
-	else:
-		if ((fila_cambio_a_numero[fila] >= 1 and fila_cambio_a_numero[fila] <=9 ) and
-			(columna_a_num[columna] >= 1 and columna_a_num[columna] <= 9 ) and
-			(numero >= 1 and numero <= 9 )):
-
-			entrada=True
-		else:
-			entrada=False
-
-	return entrada
-###################################3
-
-def falla_casilla(fila,columna,fila_cambio_a_numero,columna_a_num):	
-	
-	if not((fila_cambio_a_numero[fila] >= 1 and fila_cambio_a_numero[fila] <=9 ) and
-		(columna_a_num[columna] >= 1 and columna_a_num[columna] <= 9 ) ):
-
-	   fallo=True
-	else:
-		fallo=False
-	return fallo
-   
-###################################3
+############# condición de ganar, completar el panel
 
 def si_completas(A,B,C,D,E,F,G,H,I,l1,l2,l3,l4,l5,l6,l7,l8,l9,r1,r2,r3,r4,r5,r6,r7,r8,r9):
 	# ordena las listas
@@ -287,104 +305,71 @@ def si_completas(A,B,C,D,E,F,G,H,I,l1,l2,l3,l4,l5,l6,l7,l8,l9,r1,r2,r3,r4,r5,r6,
 		return False
 
 ###################################3
+############## raw inputs
+
+def introduces_bien_los_raw(fila,columna,casilla,sudoku,asignacion,salir_input):
+	try:
+		# plantilla de salida
+		os.system('clear')
+		sudoku(asignacion)
+		print "salir: numero = -1"
+		numero = input("Inserta un número: ")
+		## control de errores numerico input
+		if (numero >=1 and numero <=9) or numero == -1:
+			## control de errores fila y columna raw
+			if numero != -1:
+				fila,columna,casilla,salir_input = control_casillas_originales(salir_input)	
+			else:
+				salir_input=True
+		else:
+			print "número incorrecto."
+			sleep (3)
+	except :
+		print("Oops! No era válido. Intente nuevamente...")
+		sleep (3)
+
+	return fila,columna,casilla,salir_input,numero
 ###################################3
-###################################3
-###################################3
+########### condicion para poder asignar
 ##
-def condiciones_de_entrada(salir,numero,fila,columna,si_completas,sudoku,condicion_entrada,fila_cambio_a_numero,columna_a_num,asignacion):
+def condiciones_de_entrada(salir,numero,fila,columna,casilla,si_completas,sudoku,fila_cambio_a_numero,columna_a_num,asignacion):
 	
 	# cuando todas las listas esten ordenadas del 1-9 ganas.
 	if si_completas(A,B,C,D,E,F,G,H,I,l1,l2,l3,l4,l5,l6,l7,l8,l9,r1,r2,r3,r4,r5,r6,r7,r8,r9):
 		os.system('clear')
-		print "¡¡ Felicidades !! Sin duda no tiene amigos."
+		print "¡¡ Felicidades !! Sin duda no tienes amigos."
 		salir = True
-		bucle=True
+		
 	else:
-		# plantilla de salida
-		sudoku(asignacion)
-		print "salir: numero = -1"
 	# control de error input
 		salir_input=False
 		while salir_input == False:
-			try:
-				numero = input("Inserta un número: ")
-				salir_input=True
-			except :
-				 print("Oops! No era válido. Intente nuevamente...")
-	#######
-		salir_raw=False
-		while salir_raw == False:
-			if numero != -1:
-				fila,columna,casilla,salir_raw = control_casillas_originales(salir_raw)	
-			else:
-				salir_raw=True
-		
-		bucle=False
-		while bucle == False:
+			fila,columna,casilla,salir_input,numero= introduces_bien_los_raw(fila,columna,casilla,sudoku,asignacion,salir_input)
 			
-			if numero == -1:
-				os.system('clear')
-				print "Adios"
-				salir = True
-				bucle = True
-		# si quieres jugar
-			# condición para jugar (poner casilla y numero correcto)
-			elif  condicion_entrada(fila,numero,columna,fila_cambio_a_numero,columna_a_num):
-				bucle = True
-				
-			else:
-		# posibles fallos
-		############ revisar
-				os.system('clear')
-				if not (fila in fila_cambio_a_numero ):
-					print "fila alfabética incorrecta"
-				if not (columna in columna_a_num ):
-					print "Columna numérica incorrecta"
-				if numero < 1 or numero > 9 :
-					print "número erroneo"
-			###############3
-				sleep (4)
-				os.system('clear')
-
-				sudoku(asignacion)
-				print "salir: numero = -1"
-			# control de error input
-				salir_input=False
-				while salir_input == False:
-					try:
-						numero = input("Inserta un número: ")
-						salir_input=True
-					except :
-						 print("Oops! No era válido. Intente nuevamente...")
-			
-				salir_raw=False
-				while salir_raw == False:
-					if numero != -1:
-						fila,columna,casilla,salir_raw = control_casillas_originales(salir_raw)	
-					else:
-						salir_raw=True
-				
-				
+                   ###################################3		
 	
+		## CONDICION SALIR JUEGO
+		if numero == -1:
+			os.system('clear')
+			print "¡¡Adios Perdedor!!"
+			salir = True
+			
 	return salir,numero,fila,columna,casilla
 
 ###################################3
-###################################3
-salir = False
-numero=0
-fila=20
-columna=20
-
+###################################3###################################3
+###################################3###################################3
+###################################3   JUEGO ###################################3
 
 # condiciones de entrada
-salir,numero,fila,columna,casilla = condiciones_de_entrada(salir,numero,fila,columna,si_completas,sudoku,condicion_entrada,fila_cambio_a_numero,columna_a_num,asignacion)
+salir,numero,fila,columna,casilla = condiciones_de_entrada(salir,numero,fila,columna,casilla,si_completas,sudoku,fila_cambio_a_numero,columna_a_num,asignacion)
 
 ##
-salir = False
+#salir = False
 while salir == False:
 	# asignaciones añade el numero a las filas y casilla
 	lista_columna,lista_fila,fila_cambio_a_numero,columna_a_num,asignacion,salir = asignaciones(numero,fila,columna,casilla,lista_columna,lista_fila,fila_cambio_a_numero,columna_a_num,asignacion,salir)
 
-	salir,numero,fila,columna,casilla = condiciones_de_entrada(salir,numero,fila,columna,si_completas,sudoku,condicion_entrada,fila_cambio_a_numero,columna_a_num,asignacion)
+	salir,numero,fila,columna,casilla = condiciones_de_entrada(salir,numero,casilla,fila,columna,si_completas,sudoku,fila_cambio_a_numero,columna_a_num,asignacion)
 	
 
